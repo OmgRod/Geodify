1d9ca04526cd2b5c8ba5dedeb13595847ae1b8e9
Viper <momo2010sofia@gmail.com>
2024-06-29 14:15:26 +0100

    SwelvyBG

---
+++ src/modify/MenuLayer.cpp
@@ -0,0 +1,31 @@
+#include "../SwelvyBG.hpp"
+#include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(MenuLayer) {
+	bool init() {
+        static void onModify(auto& self) {
+        (void)self.setHookPriority("MenuLayer::init", -2); // For SwelvyBGInsert Aka BetterMenu no overlap
+        }
+		if (!MenuLayer::init()) {
+			return false;
+		}
+        
+        if (this->getChildByID("SwelvyBG")) {
+            return true;
+        }
+
+		if (CCNode* mainmenu = this->getChildByIDRecursive("main-menu-bg")) { 
+            mainmenu->setVisible(false); 
+        }
+		SwelvyBG* swelvyBG = SwelvyBG::create();
+		swelvyBG->setZOrder(-3);
+		swelvyBG->setID("swelvy-background");
+
+    	this->addChild(swelvyBG);
+
+		return true;
+	}
+};
