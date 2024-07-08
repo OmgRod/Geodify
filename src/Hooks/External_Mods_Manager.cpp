7690fdef65b9580897df66215b02ccc681379d38
Viper <momo2010sofia@gmail.com>
2024-07-08 18:04:10 +0100

    Demangle

--- src/Hooks/External_Mods_Manager.cpp
+++ src/Hooks/External_Mods_Manager.cpp
@@ -20,47 +20,45 @@ void runhooks(CCNode* Send,const std::string& layer) {
         }
     }
 
-std::string demangle(const char* mangledName) {
-    #if defined(GEODE_IS_ANDROID)
-    static std::unordered_map<std::string, std::string> cache;
-    
+#if defined(GEODE_IS_ANDROID)
+    std::string demangle(const char* mangledName) {
+        static std::unordered_map<std::string, std::string> cache;
+        
 
-    auto it = cache.find(mangledName);
-    if (it != cache.end()) {
-        return it->second;
+        auto it = cache.find(mangledName);
+        if (it != cache.end()) {
+            return it->second;
+        }
+
+        int status = 0;
+        char* demangled = abi::__cxa_demangle(mangledName, nullptr, nullptr, &status);
+        std::string result;
+        
+        if (status == 0) {
+            result = demangled;
+        } else {
+            result = mangledName;
+        }
+
+        free(demangled);
+
+
+        cache[mangledName] = result;
+        return result;
     }
-
-    int status = 0;
-    char* demangled = abi::__cxa_demangle(mangledName, nullptr, nullptr, &status);
-    std::string result;
-    
-    if (status == 0) {
-        result = demangled;
-    } else {
-        result = mangledName;
-    }
-
-    free(demangled);
-
-
-    cache[mangledName] = result;
-    return result;
-    #else
-    return mangledName;
-    #endif
-}
+#endif
 
 std::string getclass(auto object) {
 const char* className = typeid(*object).name();
-#if defined(GEODE_IS_WINDOWS)
+#if defined(GEODE_IS_ANDROID)
+    return demangle(className);
+#else
      size_t pos = fc.find("class ");
             if (pos != std::string::npos) {
                 return fc.substr(pos + 6);
             } else {
-                return fc
+                return fc;
             }
-#elif defined(GEODE_IS_ANDROID)
-
 #endif
 
 
@@ -77,10 +75,7 @@ class $modify(CCDirector) {
             return;
         }
         if (CCLayer* child = getChildOfType<CCLayer>(scene, 0)) {
-            const char* className = typeid(*child).name();
-            log::debug("ClassName {}", demangle(className) );
-            child->setID(demangle(className)); // prob break something but will show me how it works
-            std::string fc = className;
+            std::string fc = getclass(child);
             size_t pos = fc.find("class ");
             if (pos != std::string::npos) {
                 std::string tcn = fc.substr(pos + 6);
