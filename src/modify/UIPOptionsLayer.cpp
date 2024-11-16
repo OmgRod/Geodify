9f9ad5133893f8aa0a5fc29f3ef40260a109f2bd
OmgRod <rlimafonseca2010@gmail.com>
2024-11-16 10:55:12 +0000

    temp removed UIPOptionsLayer

--- src/modify/UIPOptionsLayer.cpp
+++ src/modify/UIPOptionsLayer.cpp
@@ -1,22 +1,22 @@
-#include "../SwelvyBG.hpp"
-#include <Geode/Geode.hpp>
-#include <Geode/modify/UIPOptionsLayer.hpp>
+// #include "../SwelvyBG.hpp"
+// #include <Geode/Geode.hpp>
+// #include <Geode/modify/UIPOptionsLayer.hpp>
 
-using namespace geode::prelude;
+// using namespace geode::prelude;
 
-class $modify(MyUIPOptionsLayer, UIPOptionsLayer) {
-	bool init() {
-		if (!UIPOptionsLayer::init()) {
-			return false;
-		}
-		if (Mod::get()->getSettingValue<bool>("show-control-settings")){
-			this->setOpacity(0);
-			auto swelvyBG = SwelvyBG::create();
-			swelvyBG->setZOrder(-3);
-			swelvyBG->setID("swelvy-background");
+// class $modify(MyUIPOptionsLayer, UIPOptionsLayer) {
+// 	bool init() {
+// 		if (!UIPOptionsLayer::init()) {
+// 			return false;
+// 		}
+// 		if (Mod::get()->getSettingValue<bool>("show-control-settings")){
+// 			this->setOpacity(0);
+// 			auto swelvyBG = SwelvyBG::create();
+// 			swelvyBG->setZOrder(-3);
+// 			swelvyBG->setID("swelvy-background");
 
-			this->addChild(swelvyBG);
-		}
-		return true;
-	}
-};
+// 			this->addChild(swelvyBG);
+// 		}
+// 		return true;
+// 	}
+// };
