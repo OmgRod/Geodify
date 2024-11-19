2020958ed26b535e59fef236b5832b3f089b3ade
OmgRod <rlimafonseca2010@gmail.com>
2024-11-19 20:52:12 +0000

    geode theme geodify

--- src/modify/geode.loader/ModsLayer.cpp
+++ src/modify/geode.loader/ModsLayer.cpp
@@ -1,4 +1,5 @@
 #include <Geode/Geode.hpp>
+#include <Geode/loader/Loader.hpp>
 #include "../../SwelvyBG.hpp"
 #include "../../Hooks/Hooker.hpp"
 
@@ -6,14 +7,16 @@ using namespace geode::prelude;
 
 class ModsLayer : public Betterhook::HookBetter { 
     void init(CCNode* _This) override {
-         if (auto bg = _This->getChildByID("bg")) {
-            bg->setVisible(false);
-        }
+        if (!(Loader::get()->getSettingValue<bool>("enable-geode-theme"))) {
+            if (auto bg = _This->getChildByID("bg")) {
+                bg->setVisible(false);
+            }
 
-        SwelvyBG* swelvyBG = SwelvyBG::create();
-        swelvyBG->setZOrder(-1);
-        swelvyBG->setID("swelvy-background");
-        _This->addChild(swelvyBG);
+            SwelvyBG* swelvyBG = SwelvyBG::create();
+            swelvyBG->setZOrder(-1);
+            swelvyBG->setID("swelvy-background");
+            _This->addChild(swelvyBG);
+        }
     }
 
     const char* PutLayer() const override { return "ModsLayer"; }
