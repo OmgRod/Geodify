816c02ee09a031a30aa4138b0f5aa2f520b11f74
OmgRod <89850217+OmgRod@users.noreply.github.com>
2025-02-07 21:43:08 +0000

    update

---
+++ src/modify/teamtcm.geometry-dash-odyssey/OdysseyComicLayer.cpp
@@ -0,0 +1,7 @@
+#include <Geode/Geode.hpp>
+#include "../../SwelvyBG.hpp"
+#include <alphalaneous.alphas_geode_utils/include/NodeModding.h>
+
+using namespace geode::prelude;
+
+SET_SWELVY(OdysseyComicLayer, "teamtcm.geometry-dash-odyssey/OdysseyComicLayer", "teamtcm.geometry-dash-odyssey/background");
---
+++ src/modify/teamtcm.geometry-dash-odyssey/OdysseySelectLayer.cpp
@@ -0,0 +1,7 @@
+#include <Geode/Geode.hpp>
+#include "../../SwelvyBG.hpp"
+#include <alphalaneous.alphas_geode_utils/include/NodeModding.h>
+
+using namespace geode::prelude;
+
+SET_SWELVY_SPRITE(OdysseySelectLayer, "teamtcm.geometry-dash-odyssey/OdysseySelectLayer");
