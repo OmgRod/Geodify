3537a0c984e3b19869f3adb5b50b597289757f92
OmgRod <89850217+OmgRod@users.noreply.github.com>
2025-02-08 16:40:28 +0000

    update

--- src/modify/gd/GJShopLayer.cpp
+++ src/modify/gd/GJShopLayer.cpp
@@ -1,7 +1,75 @@
 #include <Geode/Geode.hpp>
 #include "../../SwelvyBG.hpp"
-#include <alphalaneous.alphas_geode_utils/include/NodeModding.h>
+#include <Geode/modify/GJShopLayer.hpp>
 
 using namespace geode::prelude;
 
-SET_SWELVY_SPRITE(GJShopLayer, "gd/GJShopLayer");
+class $modify(MyGJShopLayer, GJShopLayer) {
+	bool init(ShopType p0) {
+		if (!GJShopLayer::init(p0)) {
+			return false;
+		}
+        if (p0 == ShopType::Normal) {
+            if (Mod::get()->getSettingValue<bool>("gd/GJShopLayer-Normal")){
+                if (auto bg = this->getChildByID("background")) {
+                    bg->setVisible(false);
+                }
+
+                auto swelvyBG = SwelvyBG::create();
+                swelvyBG->setZOrder(-999);
+                
+
+                this->addChild(swelvyBG);
+            }
+        } else if (p0 == ShopType::Secret) {
+            if (Mod::get()->getSettingValue<bool>("gd/GJShopLayer-Secret")){
+                if (auto bg = this->getChildByID("background")) {
+                    bg->setVisible(false);
+                }
+
+                auto swelvyBG = SwelvyBG::create();
+                swelvyBG->setZOrder(-999);
+                
+
+                this->addChild(swelvyBG);
+            }
+        } else if (p0 == ShopType::Community) {
+            if (Mod::get()->getSettingValue<bool>("gd/GJShopLayer-Community")){
+                if (auto bg = this->getChildByID("background")) {
+                    bg->setVisible(false);
+                }
+
+                auto swelvyBG = SwelvyBG::create();
+                swelvyBG->setZOrder(-999);
+                
+
+                this->addChild(swelvyBG);
+            }
+        } else if (p0 == ShopType::Mechanic) {
+            if (Mod::get()->getSettingValue<bool>("gd/GJShopLayer-Mechanic")){
+                if (auto bg = this->getChildByID("background")) {
+                    bg->setVisible(false);
+                }
+
+                auto swelvyBG = SwelvyBG::create();
+                swelvyBG->setZOrder(-999);
+                
+
+                this->addChild(swelvyBG);
+            }
+        } else if (p0 == ShopType::Diamond) {
+            if (Mod::get()->getSettingValue<bool>("gd/GJShopLayer-Diamond")){
+                if (auto bg = this->getChildByID("background")) {
+                    bg->setVisible(false);
+                }
+
+                auto swelvyBG = SwelvyBG::create();
+                swelvyBG->setZOrder(-999);
+                
+
+                this->addChild(swelvyBG);
+            }
+        }
+		return true;
+	}
+};
