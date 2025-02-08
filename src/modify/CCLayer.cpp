d8cfa5b8ae6ac77262dd145f85d4beed5f87607b
OmgRod <89850217+OmgRod@users.noreply.github.com>
2025-02-08 14:37:23 +0000

    fix

--- src/modify/CCLayer.cpp
+++ src/modify/CCLayer.cpp
@@ -7,9 +7,23 @@ using namespace geode::prelude;
 class $modify(MyCCLayer, CCLayer) {
 	void onEnter() {
 		CCLayer::onEnter();
-
 		if (auto parent = dynamic_cast<UIOptionsLayer*>(this->getParent())) {
 			if (Mod::get()->getSettingValue<bool>("gd/UIOptionsLayer")) {
+				int lowestZ = INT_MAX;
+				CCObject* obj = nullptr;
+				CCARRAY_FOREACH(this->getChildren(), obj) {
+					if (auto node = dynamic_cast<CCNode*>(obj)) {
+						lowestZ = std::min(lowestZ, node->getZOrder());
+					}
+				}
+
+				auto swelvyBG = SwelvyBG::create();
+				swelvyBG->setZOrder(lowestZ - 1);
+				this->addChild(swelvyBG);
+			}
+		}
+		if (auto parent = dynamic_cast<UIPOptionsLayer*>(this->getParent())) {
+			if (Mod::get()->getSettingValue<bool>("gd/UIPOptionsLayer")) {
 				int lowestZ = INT_MAX;
 				CCObject* obj = nullptr;
 				CCARRAY_FOREACH(this->getChildren(), obj) {
