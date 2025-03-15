9660d04a0c859b1abe83268e1cd214a791706079
OmgRod <89850217+OmgRod@users.noreply.github.com>
2025-03-15 13:38:56 +0000

    Create lblazen.gdps_hub/GDPSHubLayer.cpp

---
+++ src/modify/lblazen.gdps_hub/GDPSHubLayer.cpp
@@ -0,0 +1,7 @@
+#include <Geode/Geode.hpp>
+#include "../../SwelvyBG.hpp"
+#include <alphalaneous.alphas_geode_utils/include/NodeModding.h>
+
+using namespace geode::prelude;
+
+SET_SWELVY(GDPSHubLayer, "lblazen.gdps_hub/GDPSHubLayer", "background");
