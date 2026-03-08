ea7d4d9182a6a38aa37c9faaafb5c11fcb9ca6e8
OmgRod <89850217+OmgRod@users.noreply.github.com>
2026-03-08 11:43:05 +0000

    fixed settings menu!!!

--- src/CustomSettingLayer.cpp
+++ src/CustomSettingLayer.cpp
@@ -6,14 +6,43 @@
 using namespace geode::prelude;
 
 $on_mod(Loaded) {
-    auto listener = ModPopupUIEvent().listen([](FLAlertLayer* popup, std::string_view, std::optional<Mod*> mod) {
-        if (mod && mod.value()->getID() == Mod::get()->getID()) {
-            if (popup) {
-                if (CCMenuItemSpriteExtra* btn = typeinfo_cast<CCMenuItemSpriteExtra*>(popup->getChildByIDRecursive("settings-button"))) {
-                    btn->m_pfnSelector = menu_selector(GYSettingSelectLayer::settingsBtn);
-                }
-            }
+    ModPopupUIEvent().listen([](FLAlertLayer* popup, std::string_view, std::optional<Mod*> mod) {
+        log::debug("geodify popup event is being received");
+
+        if (!mod.has_value()) {
+            log::warn("geodify mod is not provided, using fallback");
+            mod = Mod::get();
         }
+
+        auto* modPtr = mod.value();
+        if (!modPtr) {
+            log::error("geodify mod pointer is null");
+            return ListenerResult::Propagate;
+        }
+
+        if (modPtr->getID() != Mod::get()->getID()) {
+            log::error("geodify mod ID does not match");
+            return ListenerResult::Propagate;
+        }
+
+        log::debug("geodify mod verified");
+
+        if (!popup) {
+            log::error("geodify popup is null");
+            return ListenerResult::Propagate;
+        }
+
+        log::debug("geodify popup verified");
+
+        auto* btn = typeinfo_cast<CCMenuItemSpriteExtra*>(popup->getChildByIDRecursive("settings-button"));
+        if (!btn) {
+            log::error("geodify button not found");
+            return ListenerResult::Propagate;
+        }
+
+        log::debug("geodify button found");
+        btn->m_pfnSelector = menu_selector(GYSettingSelectLayer::settingsBtn);
+
         return ListenerResult::Propagate;
-    });
+    }).leak();
 }
